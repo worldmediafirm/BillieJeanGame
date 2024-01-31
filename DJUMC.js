3e025d55fe76d097af1040d769603c6f63d1ebd9
World Media Firm <worldmediafirm@gmail.com>
2024-01-31 09:25:16 -0500

    current state of game code; adding user pic features but not done. Migrating to new computer

--- DJUMC.js
+++ DJUMC.js
@@ -1,31 +1,41 @@
-var DJUMC = document.querySelector('.DJUMC');
+var DJUMC = document.querySelector('.Main_Character');
+//var imageInput = document.getElementById('imageInput');
 
 var x_start = 0;
 var x_offset = 0;
 var isDragging = false;
 
+// Dragging Event Listeners
 DJUMC.addEventListener('touchstart', function(e) {
-  isDragging = true;
-  x_start = e.touches[0].clientX;
-  x_offset = x_start - DJUMC.offsetLeft;
+    isDragging = true;
+    x_start = e.touches[0].clientX;
+    x_offset = x_start - DJUMC.offsetLeft;
 });
 
 document.addEventListener('touchmove', function(e) {
-  if (!isDragging) return;
-  
-  var x_current = e.touches[0].clientX;
-  var left = x_current - x_offset;
+    if (!isDragging) return;
+    
+    var x_current = e.touches[0].clientX;
+    var left = x_current - x_offset;
 
-  DJUMC.style.left = left + 'px';
-
-  hitBoxLeft = DJUMC.offsetLeft;
-  hitBoxTop = DJUMC.offsetTop;
-  hitBoxRight = DJUMC.offsetLeft + DJUMC.offsetWidth;
-  hitBoxBottom = DJUMC.offsetTop + DJUMC.offsetHeight;
-
- 
+    DJUMC.style.left = left + 'px';
 });
 
 document.addEventListener('touchend', function() {
-  isDragging = false;
+    isDragging = false;
 });
+
+/*// Image Upload Handling
+imageInput.addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+        const newImageUrl = e.target.result;
+        DJUMC.style.backgroundImage = `url(${newImageUrl})`;
+    };
+
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+});*/
