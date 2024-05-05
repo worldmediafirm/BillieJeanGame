5fb50494b047846c3a79085cf08444099ecda322
WMF DevOps <worldmediafirm@gmail.com>
2024-05-05 13:00:45 -0400

    Game Starter via the SoundOn button on index.html

---
+++ game-start-module.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const startButton = document.querySelector('.Start_Button');
+    if (startButton) {
+        startButton.addEventListener('click', function() {
+            window.location.href = 'Billie Jean.html'; // Path to your Billie Jean.html file
+        });
+    }
+});
